6a297c85a4672e29c1d3e7c9238f6c4399b5c8e5
pikesley <sam.pikesley@gmail.com>
2021-10-05 11:01:03 +0100

    Enable YAML for data

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
-module.exports = function(eleventyConfig) {
+const yaml = require("js-yaml");
+
+module.exports = function (eleventyConfig) {
   // Return your Object options:
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
   return {
     dir: {
       input: "src",
-      output: "public"
-    }
-  }
+      output: "public",
+    },
+  };
 };
